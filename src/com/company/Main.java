a5873e2dea3bde14f30334968f1b604d022e8331
Andrius <andriuscere@gmail.com>
2018-01-30 15:43:14 +0200

    Uzduotis6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Įveskite savo svorį KG");
+	float a = sc.nextFloat();
+	System.out.println("Įveskite savo ūgį M");
+	float b = sc.nextFloat();
+	System.out.println("Jūsų KMI: " + (a/(b*b)));
     }
 }
